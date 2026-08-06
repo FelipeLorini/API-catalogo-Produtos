977b2f3fc53c4f38959e4696ce9adee5fc015254
FelipeLorini <felipelorini11@gmail.com>
2026-08-06 16:52:46 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD ? '******' : 'NAO DEFINIDA');
+
 const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
